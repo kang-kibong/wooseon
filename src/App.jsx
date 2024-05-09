6cbddfbdb0571215c521d2f506eeb6b9b0012e51
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-09 15:03:02 +0900

    feat: add RouteChangeTracker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import RouteChangeTracker from "./components/RouteChangeTracker";
 
 function App() {
+  RouteChangeTracker();
   return (
-    <>
-      <Home></Home>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
   );
 }
 
