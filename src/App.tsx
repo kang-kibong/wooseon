ab0f378025dd6b86353bbc14102e3db2da7bf9f5
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-17 20:33:35 +0900

    feat: add ThemeProvider and theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RouteChangeTracker from "./hooks/RouteChangeTracker";
+import { ThemeProvider } from "styled-components";
+import theme from "./assets/styles/theme";
 
 function App() {
   RouteChangeTracker();
-  
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const pallete = {
+  blue: "#007AFF",
+  "light-blue": "#F2F8FF",
+  gray: "#5F5F5F",
+  "light--black": "#1D1D27",
+  navy: "rgba(16, 61, 108, 1)",
+  white: "#fff",
+  
+};
+
+const fontSize = {
+  m_h1: "28px",
+  m_p: "18px",
+  t_h1: "36px",
+  t_p: "22px",
+  d_h1: "51px",
+  d_h2: "36px",
+  d_p: "24px",
+};
+
+const theme = {
+  pallete,
+  fontSize,
+};
+
+export default theme;
