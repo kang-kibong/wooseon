4c80c15dfcc3c3fd0dfa91d2d196ecf82b395d1d
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-17 20:46:16 +0900

    style: change theme to Guidance component style

--- src/components/home/Guidance/Guidance.styled.ts
+++ src/components/home/Guidance/Guidance.styled.ts
@@ -2,12 +2,11 @@ import styled from "styled-components";
 
 export const Layout = styled.section<{ $image: string }>`
   padding: 250px 0 250px 0;
-  /* background-color: #1d1d27; */
   background-image: url(${({ $image }) => $image});
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  color: #fff;
+  color: ${({ theme }) => theme.pallete.white};
 
   @media screen and (max-width: 1023px) {
     padding: 150px 0 150px 0;
@@ -26,33 +25,33 @@ export const Inner = styled.section`
 `;
 
 export const Title = styled.h1`
-  font-size: 51px;
+  font-size: ${({ theme }) => theme.fontSize.d_h1};
   font-weight: 800;
   margin-bottom: 32px;
   line-height: 44px;
 
   @media screen and (max-width: 1023px) {
     margin-bottom: 16px;
-    font-size: 36px;
+    font-size: ${({ theme }) => theme.fontSize.t_h1};
   }
 
   @media screen and (max-width: 540px) {
-    font-size: 28px;
+    font-size: ${({ theme }) => theme.fontSize.m_h1};
   }
 `;
 
 export const Description = styled.p`
-  font-size: 24px;
+  font-size: ${({ theme }) => theme.fontSize.d_p};
   font-weight: 500;
   margin-bottom: 42px;
 
   @media screen and (max-width: 1023px) {
-    font-size: 22px;
+    font-size: ${({ theme }) => theme.fontSize.t_p};
     margin-bottom: 34px;
   }
 
   @media screen and (max-width: 540px) {
-    font-size: 18px;
+    font-size: ${({ theme }) => theme.fontSize.m_p};
     margin-bottom: 28px;
   }
 `;
