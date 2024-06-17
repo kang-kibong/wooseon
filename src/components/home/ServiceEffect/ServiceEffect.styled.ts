f04601f4ed17d86cdb772f9ddf916497e4ea8169
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-17 20:55:25 +0900

    style: change theme to ServiceEffect component style

--- src/components/home/ServiceEffect/ServiceEffect.styled.ts
+++ src/components/home/ServiceEffect/ServiceEffect.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Layout = styled.section`
   padding: 120px 0 120px 0;
   line-height: 44px;
-  background-color: #f2f8ff;
+  background-color: ${({ theme }) => theme.pallete["light-blue"]};
 
   @media screen and (max-width: 1023px) {
     padding: 80px 0 80px 0;
@@ -11,34 +11,34 @@ export const Layout = styled.section`
 `;
 
 export const Title = styled.h1`
-  font-size: 36px;
+  font-size: ${({ theme }) => theme.fontSize.t_h1};
   font-weight: 800;
   line-height: 44px;
   margin-bottom: 16px;
   text-align: center;
 
   @media screen and (max-width: 540px) {
-    font-size: 28px;
+    font-size: ${({ theme }) => theme.fontSize.m_h1};
   }
 `;
 
 export const Description = styled.p`
-  font-size: 24px;
-  color: #5f5f5f;
+  font-size: ${({ theme }) => theme.fontSize.d_p};
+  color: ${({ theme }) => theme.pallete.gray};
   font-weight: 500;
   text-align: center;
   margin-bottom: 56px;
 
   @media screen and (max-width: 1023px) {
-    font-size: 22px;
+    font-size: ${({ theme }) => theme.fontSize.t_p};
   }
 
   @media screen and (max-width: 540px) {
-    font-size: 18px;
+    font-size: ${({ theme }) => theme.fontSize.m_p};
     margin-bottom: 48px;
   }
 `;
 
 export const Strong = styled.strong`
-  color: #007aff;
+  color: ${({ theme }) => theme.pallete.blue};
 `;
